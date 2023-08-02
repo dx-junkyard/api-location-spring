601bd7b0b90d7fcb33cbb06a71452ab21b37e2e8
urashin <s.urakawa@hotmail.co.jp>
2023-08-02 21:53:26 +0900

    fix: test

--- src/main/java/com/dxjunkyard/location/config/SwaggerConfiguration.java
+++ src/main/java/com/dxjunkyard/location/config/SwaggerConfiguration.java
@@ -7,6 +7,8 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,7 +23,8 @@ public class SwaggerConfiguration {
 		.protocols(new HashSet<>(Arrays.asList("https")))
                 .groupName("api-location-spring")    // APIドキュメントをグルーピングするための識別名
                 .select()
-                .paths(paths())
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
